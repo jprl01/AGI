c8c82135c7f9b8eaa1c47ef84e31eb077f99a76e
BernardoRibeiro <bernardoribeiro182@gmail.com>
2023-09-27 15:48:37 +0100

    Setup of frontend

---
+++ frontend/src/services/RemoteServices.ts
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import type { AxiosResponse } from 'axios';
+
+const httpClient = axios.create();
+httpClient.defaults.timeout = 50000;
+httpClient.defaults.baseURL = import.meta.env.VITE_ROOT_API;
+httpClient.defaults.headers.post['Content-Type'] = 'application/json';
+httpClient.interceptors.request.use(
+  // TODO: don't send token for ALL requests, filter by target host
+  // only send to backend-bound requests
+
+  (config) => {
+    // if (!config.headers.Authorization) {
+    //   const token = useAuthStore().token;
+    //   if (token) {
+    //     config.headers.Authorization = `Bearer ${token}`;
+    //   }
+    // }
+
+    return config;
+  },
+  (error: any) => Promise.reject(error)
+);
+
+export default class RemoteServices {
+
+  
+    static async demoAdminLogin(): Promise<String> {
+      return httpClient.get('/api').then(() => {
+        return "Hello World";
+      });
+    }
+}
